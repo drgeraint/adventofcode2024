55f1f64aa89159b842ff7407c3c4f1145b49e435
Geraint <geraint@enchant.me.uk>
2024-12-11 00:08:01 +0000

    Day 10

--- day10/day10.py
+++ day10/day10.py
@@ -12,7 +12,8 @@ ncols = len(lines[0])
 grid  = dict()
 zeros = set()
 nines = set()
-trail = dict()
+trail = dict()                  # Part 1
+paths = dict()                  # Part 2
 
 def follow(start,pos):
     val = grid[pos]
@@ -27,6 +28,20 @@ def follow(start,pos):
                 if grid[nxt] == val+1:
                     follow(start,nxt)
                 
+def follow2(start,pos,path):
+    val = grid[pos]
+    if val == 9:
+        paths[start].add(tuple(path))
+    else:
+        r = pos[0]
+        c = pos[1]
+        for nxt in [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]:
+            if (nxt[0] >= 0 and nxt[0] < nrows and
+                nxt[1] >= 0 and nxt[1] < ncols):
+                if grid[nxt] == val+1:
+                    path.append(pos)
+                    follow2(start,nxt,path)
+                
                 
 for row in range(0,nrows):
     for col in range(0,ncols):
@@ -35,6 +50,7 @@ for row in range(0,nrows):
         if c == 0:
             zeros.add((row,col))
             trail[(row,col)] = set()
+            paths[(row,col)] = set()
         elif c == 9:
             nines.add((row,col))
                   
@@ -45,4 +61,12 @@ answer1 = 0
 for z in trail:
     answer1 += len(trail[z])
 print(answer1)
+
+for z in zeros:
+    follow2(z,z,[])
+
+answer2 = 0
+for z in paths:
+    answer2 += len(paths[z])
+print(answer2)
 

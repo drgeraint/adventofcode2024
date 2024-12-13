b02519b9c2fbc6e90e345d58b996104c706d8116
Geraint <geraint@enchant.me.uk>
2024-12-13 11:28:42 +0000

    Day 13 part 1 using scipy.optimize.milp works for part 1 but not part 2

--- day13/day13.py
+++ day13/day13.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
 import numpy as np
+import scipy.optimize as opt
 
-#filename = 'test1.txt'
-filename = 'input.txt'
+filename = 'test1.txt'
+#filename = 'input.txt'
 
 with open(filename, 'r') as fin:
     lines = fin.read().splitlines()
@@ -22,32 +23,16 @@ for line in lines:
         elif label == 'Prize':
             r1, r2 = x1, x2
             R = (r1,r2)
-            #L = np.array([[l11,l12],[l21,l22]])
-            #R = np.array([r1,r2])
             games.append((L,R))
 
 wins = []
 for game in games:
     L,R = game
-    maxA = min([99,
-                int((R[0]+1)/L[0][0]),
-                int((R[1]+1)/L[1][0])])+1
-    maxB = min([99,
-                int((R[0]+1)/L[0][1]),
-                int((R[1]+1)/L[1][1])])+1
-    minA = max([1,
-                int(np.mod(R[0],L[0][1])/L[0][0]),
-                int(np.mod(R[1],L[1][1])/L[1][0])])-1
-    minB = max([1,
-                int(np.mod(R[0],L[0][0])/L[0][1]),
-                int(np.mod(R[1],L[1][0])/L[1][1])])-1
     best = ()
     cost = 3*R[0]+R[1]
-    minA,maxA = 0,100
-    for A in range(minA,maxA):
+    for A in range(0,100):
         B = int((R[0]-L[0][0]*A)/L[0][1])
         b = (R[1]-L[1][0]*A)/L[1][1]
-        #print(A,b1,b2)
         if B == b:
             J = 3*A+B
             if J < cost:
@@ -57,4 +42,22 @@ for game in games:
         wins.append(cost)
 
 print('Part 1:', sum(wins))
-            
+
+gain = 10000000000000
+cost = 0
+for game in games:
+    L,R = game
+    A = np.array([[L[0][0],L[0][1]],[L[1][0],L[1][1]]])
+    B = np.array([R[0],R[1]])*gain
+    C = np.array([3,1])
+    constraints = opt.LinearConstraint(A, B, B)
+    integrality = np.ones_like(C)
+    res = opt.milp(c=C, constraints=constraints, integrality=integrality)
+    print(res.x)
+    if res.x is not None:
+        cost += int(sum(C*res.x))
+
+print('Part 2:', cost)
+
+
+# Wrong answer: 321046574547349952 is too high

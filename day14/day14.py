83e6f1252b4d659e3e7ba82c2a69f9f6cd6c3ac6
Geraint <geraint@enchant.me.uk>
2024-12-15 00:06:57 +0000

    Day 14

--- day14/day14.py
+++ day14/day14.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
+import matplotlib.pyplot as plt
 import numpy as np
-import re
 
 #filename = 'test1.txt'
 filename = 'input.txt'
@@ -36,10 +36,10 @@ class Robot:
 for line in lines:
     Robot(line)
 
-for t in range(0,1):
+def part1(dt):
     Q1,Q2,Q3,Q4 = 0,0,0,0
     for r in robots:
-        r.move(100)
+        r.move(dt)
         x,y = r.p
         if x < int(ncols/2) and y < int(nrows/2):
             Q1 += 1
@@ -49,6 +49,57 @@ for t in range(0,1):
             Q3 += 1
         elif x > int(ncols/2) and y > int(nrows/2):
             Q4 += 1
+    return (Q1,Q2,Q3,Q4)
 
-answer1 = Q1*Q2*Q3*Q4
-print('Part 1:', answer1)
+Q = part1(100)
+print('Part 1:', np.product(Q))
+
+def plot(t):
+    grid = []
+    line = [' ']*ncols
+    for r in range(0,nrows):
+        grid.append(line.copy())
+    for robot in robots:
+        col,row = robot.p
+        grid[row][col] = '*'
+    for line in grid:
+        s = ''.join([c for c in line])
+        print(s)
+    print('Time:', t)
+
+def evalQ(Q):
+    X,Y = [],[]
+    for r in robots:
+        x,y = r.p
+        X.append(x)
+        Y.append(y)
+    J = (np.var(X),np.var(Y))
+    return J
+         
+# View tree without looping for my input
+robots = []
+for line in lines:
+   Robot(line)
+part1(8159)
+plot(8159)
+
+# Finding min var(x) and var(y) will work for other inputs
+data = {'Jx':[], 'Jy':[]}
+robots = []
+for line in lines:
+    Robot(line)
+for move in range(0,nrows*ncols):
+    Q = part1(1)
+    J = evalQ(Q)
+    Jx,Jy = J
+    data['Jx'].append(Jx)
+    data['Jy'].append(Jy)
+    if Jx < 400 and Jy < 400:
+        tree = move
+        plot(tree)
+print('Part 2:', tree+1)
+
+plt.plot(data['Jx'],data['Jy'],'.')
+plt.title('var(y) against var(x)')
+plt.show()
+              
